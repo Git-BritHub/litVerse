41a9b343c78aff87ff40ec447d094b7e37a6ef09
Brittany Brimley <brittanysapp@gmail.com>
2023-10-30 11:38:18 -0600

    created thoughtData.json for db

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,7 @@
 const connection = require('../config/connection');
-const { User } = require('../models')
+const { User, Thought } = require('../models')
 const userData = require('./userData.json');
+const thoughtData = require('./thoughtData.json');
 
 connection.on('error', (err) => err);
 
@@ -8,8 +9,11 @@ connection.once('open', async () => {
     console.log('connected');
     await User.deleteMany({});
     await User.create(userData);
+    await Thought.deleteMany({});
+    await Thought.create(thoughtData);
 
     console.table(userData);
+    console.table(thoughtData);
     console.info('Seeding complete! 🌱');
     process.exit(0);
 });
